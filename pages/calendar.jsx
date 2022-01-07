bbd7bb82b993f0b24807d613c5af757d843ab9a0
edouardjamieson99@gmail.com <info@fatfish.ca>
2022-01-07 16:28:48 -0500

    Travail sur le calendrier en lazyload

--- pages/calendar.jsx
+++ pages/calendar.jsx
@@ -1,40 +1,58 @@
-import moment from "moment";
+import moment, { updateLocale } from "moment";
 import { useEffect, useState } from "react";
 import Layout from "../components/Layout";
 import Modal from "../components/Modal";
 
 export default function Calendar() {
 
+    updateLocale('en', { week: { dow: 1 } })
+
+    // STATES POUR LE CALENDRIER
     const [calendar, setCalendar] = useState([])
-    const [momentValue, setMomentValue] = useState(moment().locale('fr'))
+    const [calendarMonthsTrigger, setCalendarMonthsTrigger] = useState({})
+    const [momentValue, setMomentValue] = useState(moment())
     const [newEventDay, setNewEventDay] = useState(null)
 
     const [modalVisible, setModalVisible] = useState(false)
 
-    useEffect(() => {
-        const startDay = momentValue.clone().startOf('month').startOf('week')
-        const endDay = momentValue.clone().endOf('month').endOf('week')
-        const day = startDay.clone().subtract(1, 'day')
+    let calendar_height = 0
 
-        const builder = []
-        while(day.isBefore(endDay, 'day')) {
-            builder.push(
+    useEffect(() => {
+
+        // Détermine la hauteur du calendrier 
+        setCalendarHeight()
+
+        // Récupère le début & la fin de l'année
+        const yearStart = momentValue.clone().startOf('year').startOf('week')
+        const yearEnd = momentValue.clone().endOf('year').endOf('week')
+        const day = yearStart.clone().subtract(1, 'day')
+
+        const cal = []
+        while(day.isBefore(yearEnd, 'day')) {
+            cal.push(
                 Array(7)
                 .fill(0)
                 .map(() => day.add(1, 'day').clone())
             )
         }
 
-        setCalendar(builder)
+        const triggers = {...calendarMonthsTrigger}
+        triggers[momentValue.clone().format('YYYY')] = Array(12).fill(0).map((n,i) => momentValue.clone().month(i).startOf('week').format('w'))
+        setCalendarMonthsTrigger(triggers)
 
-
+        setCalendar(cal)
+        // Le useEffect est set sur le momentValue pcq on veut tout recalculer si la valeur
+        // de moment() change (ex: changer de mois)
     }, [momentValue])
 
     // ====================================================================
     // FUNCTIONS / ACTIONS
     // ====================================================================
+    
+    //Permet de changer de mois
     const handleSwitchMonth = (direction) => {
         
+        // On set le state à la nouvelle valeur dépendant de la direction
         setMomentValue(
             direction === -1 ?
             momentValue.clone().subtract(1, 'month') :
@@ -47,20 +65,22 @@ export default function Calendar() {
     // HELPERS
     // ====================================================================
 
+    // Regarde si une journée est avant la journée d'aujourd'hui
     const isBeforeToday = (day) => {
         return day.isBefore(new Date(), 'day')
     }
-
+    // Regarde si la journée est la journée d'aujourd'hui
     const isToday = (day) => {
         return day.isSame(new Date(), 'day')
     }
-
+    // Regarde si la journée tombe une fin de semaine
     const isWeekend = (day) => {
         //0 = dimanche
         //6 = samedi
         return ["0","6"].includes(day.format('d'))
     }
 
+    // Permet de donner la bonne classe aux cellules
     const getStyles = (day) => {
         let classname = "calendar-week_day"
         if(isToday(day)) classname += " today"
@@ -68,7 +88,7 @@ export default function Calendar() {
         if(isWeekend(day)) classname += " weekend"
         return classname
     }
-
+    // Permet de convertir les mois en français
     const getMonthInFrench = (month) => {
         switch (month) {
             case "January":
@@ -98,29 +118,55 @@ export default function Calendar() {
         }
     }
 
+    const setCalendarHeight = () => {
+        calendar_height = window.innerHeight
+        - document.querySelector('.main-header').clientHeight
+        - document.querySelector('.hero').clientHeight
+        - document.querySelector('.calendar-head').clientHeight
+        - 64
+        document.documentElement.style.setProperty('--calendar-height', calendar_height)
+    }
+
+    const scrollToToday = () => {
+        const today = momentValue.clone().format('MM/DD/YYYY')
+        const element_with_date = document.querySelectorAll(`.calendar-week_day[data-date="${today}"]`)[0]
+        element_with_date.scrollIntoView({ behavior: 'smooth' })
+    }
+
     // ====================================================================
     // COMPONENTS
     // ====================================================================
 
     const Calendar = () => {
 
-        return calendar.map(week => 
-            <div className="calendar-week" key={week}>
+        return (
+            <div className="calendar-content">
                 {
-                    week.map(day => 
-                        <div
-                            className={getStyles(day)}
-                            key={day.format('D')}
-                            onDoubleClick={() => {
-                                if(!isBeforeToday(day)) {
-                                    setNewEventDay(day)
-                                    setModalVisible(true)
-                                }
-                            }}
-                        >
+                    calendar.map((week, i) => 
+                        <div className="calendar-week" key={`trigger-${week[0].format('w')}`}>
+                            {
+                                calendarMonthsTrigger[momentValue.clone().format('YYYY')].includes(week[0].format('w')) ?
+                                <div className="calendar-week_trigger" key={`trigger-${week[0].format('w')}`}></div> : null
+                            }
+                            {
+                                week.map(day => 
+                                    <div
+                                        className={getStyles(day)}
+                                        key={day.format('MM/DD')}
+                                        data-date={day.format('MM/DD/YYYY')}
+                                        // onDoubleClick={() => {
+                                        //     if(!isBeforeToday(day)) {
+                                        //         setNewEventDay(day)
+                                        //         setModalVisible(true)
+                                        //     }
+                                        // }}
+                                    >
 
 
-                            <div className="calendar-week_day-head">{ day.format('D') }</div>
+                                        <div className="calendar-week_day-head">{ day.format('D') }</div>
+                                    </div>
+                                )
+                            }
                         </div>
                     )
                 }
@@ -131,15 +177,15 @@ export default function Calendar() {
     const CalendarPicker = () => {
         return (
             <div className="calendar-picker">
-                <button type="button" className="calendar-picker_button" onClick={() => handleSwitchMonth(-1)}>
-                    <i className="fas fa-arrow-left"></i>
-                    <span>{ getMonthInFrench(momentValue.clone().subtract(1, 'month').format('MMMM')) }</span>
-                </button>
-
                 <span className="calendar-picker_current">{ getMonthInFrench(momentValue.clone().format('MMMM')) } {momentValue.clone().format('YYYY')}</span>
                 
+                <button type="button" className="calendar-picker_button" onClick={() => handleSwitchMonth(-1)}>
+                    <i className="fas fa-arrow-left"></i>
+                </button>
+                <button type="button" className="calendar-picker_button" onClick={() => scrollToToday()}>
+                    <span>Aujourd'hui</span>
+                </button>
                 <button type="button" className="calendar-picker_button" onClick={() => handleSwitchMonth(1)}>
-                    <span>{ getMonthInFrench(momentValue.clone().add(1, 'month').format('MMMM')) }</span>
                     <i className="fas fa-arrow-right"></i>
                 </button>
             </div>
@@ -149,13 +195,16 @@ export default function Calendar() {
     const CalendarHead = () => {
         return (
             <div className="calendar-head">
-                <div className="calendar-head_day weekend">Dimanche</div>
-                <div className="calendar-head_day">Lundi</div>
-                <div className="calendar-head_day">Mardi</div>
-                <div className="calendar-head_day">Mercredi</div>
-                <div className="calendar-head_day">Jeudi</div>
-                <div className="calendar-head_day">Vendredi</div>
-                <div className="calendar-head_day weekend">Samedi</div>
+                <CalendarPicker />
+                <div className="calendar-head_days">
+                    <div className="calendar-head_day">Lundi</div>
+                    <div className="calendar-head_day">Mardi</div>
+                    <div className="calendar-head_day">Mercredi</div>
+                    <div className="calendar-head_day">Jeudi</div>
+                    <div className="calendar-head_day">Vendredi</div>
+                    <div className="calendar-head_day weekend">Samedi</div>
+                    <div className="calendar-head_day weekend">Dimanche</div>
+                </div>
             </div>
         )
     }
@@ -169,7 +218,6 @@ export default function Calendar() {
 
 
             <div className="calendar">
-                <CalendarPicker />
                 <CalendarHead />
                 <Calendar />
             </div>
